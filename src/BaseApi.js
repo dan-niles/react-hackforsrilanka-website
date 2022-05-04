40924aaee67e1ca2e822166c5c2786cf2e8e8b65
Balram <balram.wangoes@gmail.com>
2022-05-04 16:18:39 +0530

    Updated Base Url

--- src/BaseApi.js
+++ src/BaseApi.js
@@ -1,3 +1,1 @@
-export const baseURL = "https://041b-2401-4900-1c19-dbf0-55c2-8e84-3dda-26e9.ngrok.io";
-
-// export const  baseURL ="https://hfsl-backend.herokuapp.com"
+export const  baseURL ="https://hfsl-backend.herokuapp.com"
